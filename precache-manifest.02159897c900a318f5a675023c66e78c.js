106233c8a702eea30e4696025f356794d1fd20e9
mpvitalik <mpvitalik@gmail.com>
2019-08-07 18:10:47 +0300

    Updates

---
+++ precache-manifest.02159897c900a318f5a675023c66e78c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20174a6a7302c877f6a99e069a7f5c50",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "a30170aab9a9cd8e6f32",
+    "url": "/react_add-todo-form/static/css/main.8b14bee6.chunk.css"
+  },
+  {
+    "revision": "9d1094e3d6f8817a0fe4",
+    "url": "/react_add-todo-form/static/js/2.d9d1bc8f.chunk.js"
+  },
+  {
+    "revision": "a30170aab9a9cd8e6f32",
+    "url": "/react_add-todo-form/static/js/main.acd7e901.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
